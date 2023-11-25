779e7f06eb7a182961f05a6240eff84cc50f6fbc
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-25 09:18:23 -0800

    not-found file

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,6 @@
+import { notFound } from "next/navigation";
+
+
 export default function ReviewDetails({
     params,
    }: {
@@ -7,6 +10,10 @@ export default function ReviewDetails({
     };
 
    }) {
+
+    if(parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
    return (
        <h1>Review {params.reviewId} for product {params.productId}</h1>
    )
