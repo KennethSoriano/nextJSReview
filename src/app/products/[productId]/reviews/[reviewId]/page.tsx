d885010b11137ba4650e99c3b89980db12b3a895
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-24 19:43:00 -0800

    nested dynamic routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewDetails({
+    params,
+   }: {
+       params : {
+        reviewId: string;
+        productId: string;
+    };
+
+   }) {
+   return (
+       <h1>Review {params.reviewId} for product {params.productId}</h1>
+   )
+}
